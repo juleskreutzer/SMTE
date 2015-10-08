0c71e7fcebaba5bf3fa244f10d62c453d90741f1
juleskreutzer <j.kreutzer@student.fontys.nl>
2015-10-08 09:38:14 +0200

    Price Calculator initial commit

---
+++ Price Calculator/Price Calculator/Price Calculator/Colors.swift
@@ -0,0 +1,27 @@
+//
+//  Colors.swift
+//  Price Calculator
+//
+//  Created by Jules Kreutzer on 02-10-15.
+//  Copyright © 2015 Jules Kreutzer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Colors {
+    var yellow : UIColor = UIColor(red: 1, green: 0.8, blue: 0.3, alpha: 1.0)
+    var green : UIColor = UIColor(red: 0, green: 0.7, blue: 0.6, alpha: 1.0)
+    var white : UIColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+    var pink : UIColor = UIColor(red: 0.96, green: 0.35, blue: 0.42, alpha: 1.0)
+    var blue : UIColor = UIColor(red: 0.18, green: 0.20, blue: 0.31, alpha: 1.0)
+    
+    func getYellow(bla : String) -> UIColor
+    {
+        return yellow
+    }
+
+    
+}
+
---
+++ Price Calculator/Price Calculator/Price Calculator/StartViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StartViewController.swift
+//  Price Calculator
+//
+//  Created by Jules Kreutzer on 02-10-15.
+//  Copyright © 2015 Jules Kreutzer. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().barTintColor = UIColor(red: 1, green: 0.8, blue: 0.3, alpha: 1.0)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
